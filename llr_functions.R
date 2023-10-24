76b005dbde7335aefde49dd27c0d514be36eee31
Soocheol Cho <jrdrcho3@gmail.com>
2023-10-24 11:28:45 -0400

    speeding up using sweep function

--- llr_functions.R
+++ llr_functions.R
@@ -3,13 +3,22 @@ llr = function(x, y, z, omega) {
 	return(fits)
 }
 
+# compute_f_hat = function(z, x, y, omega) {
+# 	Wz = diag(make_weight_matrix(z, x, omega))
+# 	X = make_predictor_matrix(x)
+# 	Wz_x <- apply(X,2,function(x)Wz*x)
+# 	Wz_y <- Wz*y
+# 	f_hat = c(1, z) %*% solve(t(X) %*% Wz_x) %*% t(X) %*%  Wz_y
+# 	return(f_hat)
+# }
+
 compute_f_hat = function(z, x, y, omega) {
-	Wz = diag(make_weight_matrix(z, x, omega))
-	X = make_predictor_matrix(x)
-	Wz_x <- apply(X,2,function(x)Wz*x)
-	Wz_y <- Wz*y
-	f_hat = c(1, z) %*% solve(t(X) %*% Wz_x) %*% t(X) %*%  Wz_y
-	return(f_hat)
+  Wz = diag(make_weight_matrix(z, x, omega))
+  X = make_predictor_matrix(x)
+  Wz_X <- sweep(X, 2, Wz, `*`)
+  Wz_y <- Wz * y
+  f_hat = c(1, z) %*% solve(t(Wz_X) %*% X) %*% t(Wz_X) %*% Wz_y
+  return(f_hat)
 }
 
 make_predictor_matrix <- function(x) {
